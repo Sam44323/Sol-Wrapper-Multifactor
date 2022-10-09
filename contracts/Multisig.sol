11a7e7a80390dc8eb66ae5295b5db7c2a3f2b4c2
Suranjan <suranjanbiswas2002@gmail.com>
2022-10-09 17:53:05 +0530

    add: added initiater for transaction-submitter

--- contracts/Multisig.sol
+++ contracts/Multisig.sol
@@ -34,13 +34,31 @@ contract Multisig {
             _txApprovalRequired > 0 && _txApprovalRequired <= _owners.length,
             "invalid tx approval owners!"
         );
+
+        // pushing the owners to the array
         for (uint i = 0; i < owners.length; i++) {
             require(!isOwner[_owners[i]], "owner not unique!");
             require(_owners[i] != address(0), "invalid owner!");
             isOwner[owners[i]] = true;
+            owners.push(_owners[i]);
         }
-        owners = _owners;
-        txApprovalRequired = _txApprovalRequired;
+        txApprovalRequired = _txApprovalRequired; // setting the tx-required variable
+    }
+
+    receive() external payable {
+        emit Deposit(msg.sender, msg.value, address(this).balance);
+    }
+
+    function submitTransaction(
+        address _to,
+        uint _amount,
+        bytes calldata _data
+    ) external onlyOwner {
+        uint txIndex = transactions.length;
+        transactions.push(
+            Transaction({to: _to, value: _amount, data: _data, executed: false})
+        );
+        emit SubmitTransaction(msg.sender, txIndex, _to, _amount, _data);
     }
 
     modifier onlyOwner() {
