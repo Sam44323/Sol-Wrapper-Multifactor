2b031946087a30527d5daf048efe71df69ddc273
Suranjan <suranjanbiswas2002@gmail.com>
2022-10-09 15:04:16 +0530

    add: added custom docs

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -1,29 +1,33 @@
 pragma solidity ^0.8.9;
 
-contract Escrow{
-  address public escrowOwner; // deployer of the contract
-  mapping(address => uint) public deposits; // deposits of each user
-  
-  event Deposit(address from, uint amount);
-  event Withdraw(address from, uint amount);
+contract Escrow {
+    address public escrowOwner; // deployer of the contract
+    mapping(address => uint) public deposits; // deposits of each user
 
-  constructor(){
-    escrowOwner = msg.sender;
-  }
-  // receive native contract for funds (default)
-  receive() external payable{
-    emit Withdraw(msg.sender, address(this).balance);
-    deposits[msg.sender] += msg.value;
-  }
+    event Deposit(address from, uint amount);
+    event Withdraw(address from, uint amount);
 
-  // withdraw function (admin-only and destructible post withdraw)
-  function withdraw() external onlyOwner {
-    emit Withdraw(escrowOwner, address(this).balance);
-    selfdestruct(payable(escrowOwner)); // destruct the contract and send funds to owner
-  }
+    constructor() {
+        escrowOwner = msg.sender;
+    }
 
-  modifier onlyOwner(){
-    require(msg.sender == escrowOwner, "Only the owner can call this function");
-    _;
-  }
+    // receive native contract for funds (default)
+    receive() external payable {
+        emit Withdraw(msg.sender, address(this).balance);
+        deposits[msg.sender] += msg.value;
+    }
+
+    // withdraw function (admin-only and destructible[allocation_0] post withdraw)
+    function withdraw() external onlyOwner {
+        emit Withdraw(escrowOwner, address(this).balance);
+        selfdestruct(payable(escrowOwner)); // destruct the contract and send funds to owner
+    }
+
+    modifier onlyOwner() {
+        require(
+            msg.sender == escrowOwner,
+            "Only the owner can call this function"
+        );
+        _;
+    }
 }
