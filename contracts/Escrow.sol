d27ecd2bce3f92482d5580598a4bd013f59a383a
Suranjan <suranjanbiswas2002@gmail.com>
2022-10-09 14:43:10 +0530

    uupdate: updated the dependencies

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -2,16 +2,21 @@ pragma solidity ^0.8.9;
 
 contract Escrow{
   address public escrowOwner; // deployer of the contract
+  
+  event Deposit(address from, uint amount);
+  event Withdraw(address from, uint amount);
 
   constructor(){
     escrowOwner = msg.sender;
   }
-  // receive native contract for funds
+  // receive native contract for funds (default)
   receive() external payable{
+    emit Withdraw(msg.sender, address(this).balance);
   }
 
   // withdraw function (admin-only and destructible post withdraw)
   function withdraw() external onlyOwner {
+    emit Withdraw(escrowOwner, address(this).balance);
     selfdestruct(payable(escrowOwner)); // destruct the contract and send funds to owner
   }
 
