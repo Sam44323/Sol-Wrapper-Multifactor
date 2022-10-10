a0f88cf98038dd3ac5b9f78a3938f784b96f4eb2
Suranjan <suranjanbiswas2002@gmail.com>
2022-10-11 00:11:24 +0530

    add: added custom interfaces

--- contracts/Dutchauction.sol
+++ contracts/Dutchauction.sol
@@ -1,4 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+interface IERC721 {
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) external;
+}
+
 contract Dutchauction {}
