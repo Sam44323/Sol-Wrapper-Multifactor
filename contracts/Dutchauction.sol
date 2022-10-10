bc82e8eac7e7d9c95607d411dea2167a4024e683
Suranjan <suranjanbiswas2002@gmail.com>
2022-10-11 00:19:16 +0530

    add: added variable initializer for the contract

--- contracts/Dutchauction.sol
+++ contracts/Dutchauction.sol
@@ -10,6 +10,41 @@ interface IERC721 {
 }
 
 contract Dutchauction is IERC721 {
+    // contract based variables
+    uint private constant DURATION = 7 days;
+    uint public immutable startTime;
+    uint public immutable endTime;
+    uint public immutable startingPrice;
+    uint public immutable discountRate;
+
+    // nft based variables
+    IERC721 public immutable nft;
+    uint public immutable nftId;
+
+    // seller based variables
+    address public immutable seller;
+
+    constructor(
+        address _nft,
+        uint _nftId,
+        uint _startingPrice,
+        uint _discountRate,
+        uint _startTime
+    ) {
+        require(_discountRate < 100, "Discount rate must be less than 100");
+        require(
+            _startTime >= block.timestamp,
+            "Start time must be in the future"
+        );
+        nft = IERC721(_nft);
+        nftId = _nftId;
+        startingPrice = _startingPrice;
+        discountRate = _discountRate;
+        startTime = _startTime;
+        endTime = _startTime + DURATION;
+        seller = msg.sender;
+    }
+
     function transferFrom(
         address _from,
         address _to,
