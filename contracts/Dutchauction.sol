1849cdb6e92e19abf36420530e3fc74388f6d53e
Suranjan <suranjanbiswas2002@gmail.com>
2022-10-11 00:55:39 +0530

    add: added code for seller and self-destruct removal

--- contracts/Dutchauction.sol
+++ contracts/Dutchauction.sol
@@ -49,7 +49,7 @@ contract Dutchauction is IERC721 {
         seller = payable(msg.sender);
     }
 
-    function getListingPrice() external view returns (uint) {
+    function getListingPrice() public view returns (uint) {
         if (block.timestamp < startTime) {
             return startingPrice;
         } else if (block.timestamp >= endTime) {
@@ -59,6 +59,18 @@ contract Dutchauction is IERC721 {
             uint discount = (elapsedTime * discountRate) / DURATION;
             return startingPrice - discount;
         }
+    }
+
+    function buy() external payable {
+        require(block.timestamp >= startTime, "Auction not started yet!");
+        require(block.timestamp <= endTime, "Auction already ended!");
+        uint price = getListingPrice();
+        require(msg.value >= price, "Not enough funds sent!");
+        uint refundPrice = msg.value - price;
+        nft.transferFrom(seller, msg.sender, nftId);
+        if (refundPrice > 0) payable(msg.sender).transfer(refundPrice);
+        seller.transfer(msg.value);
+        selfdestruct(seller);
     }
 
     function transferFrom(
