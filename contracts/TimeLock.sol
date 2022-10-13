d5cd63e4d18c2b095c141f48243487b13b067701
Suranjan <suranjanbiswas2002@gmail.com>
2022-10-14 01:54:05 +0530

    add: added owner initializer for timelock contract

--- contracts/TimeLock.sol
+++ contracts/TimeLock.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-contract TimeLock {
-    constructor() {}
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TimeLock is Ownable {
+    address private _owner;
+
+    constructor() {
+        _owner = msg.sender;
+    }
 }
--- test/TimeLock.ts
+++ test/TimeLock.ts
@@ -2,3 +2,5 @@ import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { TimeLock } from "../typechain";
 import { expect } from "chai";
 import { ethers } from "hardhat";
+
+describe("TimeLock", () => {});
