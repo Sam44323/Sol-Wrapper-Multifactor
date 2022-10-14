53325e9e9dce05d52f51f109b26a13385594c564
Suranjan <suranjanbiswas2002@gmail.com>
2022-10-14 17:31:40 +0530

    add: added custom before-each clause selection for testers

--- test/TimeLock.ts
+++ test/TimeLock.ts
@@ -3,4 +3,24 @@ import { TimeLock, TimeLockTargetCaller } from "../typechain";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("TimeLock", () => {});
+describe("TimeLock", () => {
+  let timeLock: TimeLock,
+    timelockTester: TimeLockTargetCaller,
+    owner: SignerWithAddress;
+
+  beforeEach(async () => {
+    [owner] = await ethers.getSigners();
+
+    const TimeLock = await ethers.getContractFactory("TimeLock");
+    timeLock = (await TimeLock.deploy()) as TimeLock;
+    await timeLock.deployed();
+
+    const TimeLockTargetCaller = await ethers.getContractFactory(
+      "TimeLockTargetCaller"
+    );
+    timelockTester = (await TimeLockTargetCaller.deploy(
+      timeLock.address
+    )) as TimeLockTargetCaller;
+    await timelockTester.deployed();
+  });
+});
