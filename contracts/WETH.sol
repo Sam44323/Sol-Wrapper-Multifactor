c3805736d4f12682c7d12163d222eb4162cb3fa9
Suranjan <suranjanbiswas2002@gmail.com>
2022-10-15 00:27:50 +0530

    update: updated the decimal-value

--- contracts/WETH.sol
+++ contracts/WETH.sol
@@ -8,11 +8,11 @@ contract WETH is ERC20 {
 
     function mint() external payable {
         transferFrom(msg.sender, address(this), msg.value);
-        _mint(msg.sender, msg.value / 10**18);
+        _mint(msg.sender, msg.value);
     }
 
     function burn(uint256 amount) external {
-        _burn(msg.sender, amount / 10**18);
-        payable(msg.sender).transfer(amount / 10**18);
+        _burn(msg.sender, amount);
+        payable(msg.sender).transfer(amount);
     }
 }
--- test/WETH.ts
+++ test/WETH.ts
@@ -21,4 +21,14 @@ describe("WETH", () => {
   it("should return the correct decimals", async () => {
     expect(await weth.decimals()).to.equal(18);
   });
+
+  it("Mint some WETH token with locking", async () => {
+    await weth.mint({
+      value: (10 ** 18).toString(),
+    });
+    console.log(
+      ethers.utils.parseEther((await weth.balanceOf(owner.address)).toString())
+    );
+    expect(1000).to.equal(1000);
+  });
 });
