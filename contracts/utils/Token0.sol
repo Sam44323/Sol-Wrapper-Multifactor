a9c4c661ff8667118abf6f5881cd79ab6fee48f2
Suranjan <suranjanbiswas2002@gmail.com>
2022-10-15 22:52:40 +0530

    update: updated the logs

--- contracts/utils/Token0.sol
+++ contracts/utils/Token0.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract WETH is ERC20 {
-    constructor() ERC20("Wrapped Ether", "WETH") {}
+    constructor() ERC20("Token zero", "T0") {}
 
     function mint() external payable {
         _mint(msg.sender, msg.value);
