83406dec7db8b7928c59935da6b8f4dda2856fd8
Suranjan <suranjanbiswas2002@gmail.com>
2022-10-15 14:34:49 +0530

    add: added custom confirmation case for vault

--- test/Vault.ts
+++ test/Vault.ts
@@ -1,4 +1,21 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { Vault } from "../typechain";
+import { Vault, ChainToken } from "../typechain";
 import { expect } from "chai";
 import { ethers } from "hardhat";
+
+describe("Vault", () => {
+  let vault: Vault,
+    chainToken: ChainToken,
+    addr1: SignerWithAddress,
+    addr2: SignerWithAddress;
+
+  beforeEach(async () => {
+    const Vault = await ethers.getContractFactory("Vault");
+    const ChainToken = await ethers.getContractFactory("ChainToken");
+    chainToken = await ChainToken.deploy();
+    await chainToken.deployed();
+    [addr1, addr2] = await ethers.getSigners();
+    vault = await Vault.deploy(chainToken.address);
+    await vault.deployed();
+  });
+});
