f0c057d8faa2abae528e47faa5957d3ec1e4aa14
Suranjan <suranjanbiswas2002@gmail.com>
2022-10-15 18:44:33 +0530

    add: added token-constructor initializer for tokens

--- contracts/CPAMM.sol
+++ contracts/CPAMM.sol
@@ -4,5 +4,13 @@ pragma solidity ^0.8.9;
 import "./utils/IERC20.sol";
 
 contract CPAMM {
-    constructor() {}
+    IERC20 public immutable token0;
+    IERC20 public immutable token1;
+
+    constructor(address _token0, address _token1) {
+        // NOTE: This contract assumes that
+        // token0 and token1 both have same decimals
+        token0 = IERC20(_token0);
+        token1 = IERC20(_token1);
+    }
 }
