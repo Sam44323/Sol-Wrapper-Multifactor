07e9a277e13ca4d08c5cbe80dd50ce6ff80d4ec6
Suranjan <suranjanbiswas2002@gmail.com>
2022-10-15 22:54:18 +0530

    update: updated the codes for amm

--- contracts/CSAMM.sol
+++ contracts/CSAMM.sol
@@ -4,8 +4,14 @@ pragma solidity ^0.8.9;
 import "./utils/IERC20.sol";
 
 contract CSAMM {
-    IERC20 public immutable token0;
-    IERC20 public immutable token1;
+    IERC20 public immutable token0; // address for token0
+    IERC20 public immutable token1; // address for token1
+
+    uint public reserve0; // reseve value for token0
+    uint public reserve1; // reseve value for token1
+
+    uint public totalSupply;
+    mapping(address => uint) public balanceOf; // share balance of each liquidity-providers
 
     constructor(address _token0, address _token1) {
         // NOTE: This contract assumes that
@@ -13,4 +19,25 @@ contract CSAMM {
         token0 = IERC20(_token0);
         token1 = IERC20(_token1);
     }
+
+    function _mint(address _to, uint _amount) private {}
+
+    function _burn(address _from, uint _amount) private {}
+
+    function _update(uint _amount0, uint _amount1) private {}
+
+    function swap(address _tokenIn, address _tokenOut)
+        external
+        returns (uint amountOut)
+    {}
+
+    function addLiquidity(uint _amount0, uint _amount1)
+        external
+        returns (uint _shares)
+    {}
+
+    function removeLiquidity(uint _shares)
+        external
+        returns (uint _amount0, uint _amount1)
+    {}
 }
