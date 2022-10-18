4773cd9200f8b570aff0d276f81c6319c6a5d734
Suranjan <suranjanbiswas2002@gmail.com>
2022-10-18 16:46:34 +0530

    add: added before-each clause for factory for definition

--- test/CSAMM.ts
+++ test/CSAMM.ts
@@ -2,3 +2,24 @@ import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { CSAMM, Token0, Token1 } from "../typechain";
 import { expect } from "chai";
 import { ethers } from "hardhat";
+
+describe("CSAMM", () => {
+  let csamm: CSAMM, token0: Token0, token1: Token1, addr1: SignerWithAddress;
+
+  beforeEach(async () => {
+    const signers = await ethers.getSigners();
+    addr1 = signers[1];
+
+    const Token0 = await ethers.getContractFactory("Token0");
+    token0 = await Token0.deploy();
+    await token0.deployed();
+
+    const Token1 = await ethers.getContractFactory("Token1");
+    token1 = await Token1.deploy();
+    await token1.deployed();
+
+    const CSAMM = await ethers.getContractFactory("CSAMM");
+    csamm = await CSAMM.deploy(token0.address, token1.address);
+    await csamm.deployed();
+  });
+});
